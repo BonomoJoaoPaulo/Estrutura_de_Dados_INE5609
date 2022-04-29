70520be8e926015caa257e9a65db0e7c2ad1dacb
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-04-29 00:09:21 -0300

    Resolução do exercício 8

--- Lista_Recursao/exc8.py
+++ Lista_Recursao/exc8.py
@@ -3,7 +3,13 @@ valor par da lista.
 """
 
 
-def indice_prineiro_par(array):
-    return
+def indice_primeiro_par(array, i=0):
+    if array[i] % 2 == 0:
+        print(i)
+        return
+    indice_primeiro_par(array, i+1)
 
 
+lista_teste = [1, 3, 5, 7, 6]
+
+indice_primeiro_par(lista_teste)
