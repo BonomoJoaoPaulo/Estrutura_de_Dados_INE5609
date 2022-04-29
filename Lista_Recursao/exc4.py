2544aeaa8539a28a7d27e120bfb2454d88b2e758
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-04-29 00:39:55 -0300

    Resolução do exercício 4

--- Lista_Recursao/exc4.py
+++ Lista_Recursao/exc4.py
@@ -7,5 +7,23 @@ Exemplo de saída: False
 """
 
 
-def sequencia_crescente(sequencia):
-    return
+import math
+
+
+def sequencia_crescente(sequencia, i=0, anterior=-math.inf):
+    if i == len(sequencia):
+        print("True")
+        return
+
+    if sequencia[i] > anterior:
+        sequencia_crescente(sequencia, i+1, sequencia[i])
+    else:
+        print("False")
+        return
+
+
+lista_exemplo = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista_exemplo2 = [1, 2, 3, 4, 5, 3]
+
+sequencia_crescente(lista_exemplo)
+sequencia_crescente(lista_exemplo2)
