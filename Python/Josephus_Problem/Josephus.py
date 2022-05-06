993f9c4a30b98b6a470aa77e58e709a01518ed04
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-05-06 19:47:31 -0300

    Correção de bug -não resetava a lista encadeada

--- Python/Josephus_Problem/Josephus.py
+++ Python/Josephus_Problem/Josephus.py
@@ -7,6 +7,7 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.tail = None
+        self.size = 0
 
     def append(self, x):
         novo = Node()
@@ -18,39 +19,75 @@ class LinkedList:
             novo.next = self.tail.next
             self.tail.next = novo
 
+        self.size += 1
 
     def insert(self, x, pos):
         novo = Node()
         novo.value = x
         q = self.head
         q.pos = 0
-        while q.pos != pos-1:
+        while q.pos != pos - 1:
             q = q.next
         novo.next = q.next
         q.next = novo
 
     def remove(self, x):
-        q = self.tail
-        while q.next != self.tail:
-            if q == self.tail:
-                print('oi')
-                q.next = q.next.next
-                self.tail = self.tail.next
+        q = None
+        while True:
+            if q == None:
+                q = self.tail
+
+            elif q == self.tail:
                 break
-            q = q.next
+
+            if q.next.value == x:
+                if q.next == self.tail:
+                    self.tail = q.next.next
+                q.next = q.next.next
+                self.size -= 1
+                break
+
+            else:
+                q = q.next
+
+    def find_prox(self, m, current):
+        count = 1
+        to_kill = current
+        while count <= m:
+            to_kill = to_kill.next
+            count += 1
+        return to_kill
 
     def printa(self):
-        q = self.tail
-        print(q.value)
-        while q.next is not self.tail:
-            print(q.next.value)
+        q = None
+        while True:
+            if q == None:
+                q = self.tail
+            elif q == self.tail:
+                break
+            print(q.value)
             q = q.next
 
 
-teste = LinkedList()
-teste.append("José")
-teste.append("Matheus")
-teste.append("João")
-teste.printa()
-teste.remove("José")
-teste.printa()
+j = int(input())
+
+for count in range(j):
+    n = int(input())
+    m = int(input())
+    teste = LinkedList()
+
+    teste.append(1)
+    for i in range(n, 1, -1):
+        teste.append(i)
+
+    q = teste.tail
+
+    while teste.size != 1:
+        morto = teste.find_prox(m, q)
+        if morto == q:
+            morto = q.next
+
+        teste.remove(morto.value)
+        q = morto.next
+
+    print(f"Usando n={n}, m={m}, resultado={teste.tail.value}")
