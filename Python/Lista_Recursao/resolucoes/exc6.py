bfdf39169427198092ac5f8818b6e7e0c817a0b2
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-05-11 18:51:58 -0300

    Correção do exercício 6

--- Python/Lista_Recursao/resolucoes/exc6.py
+++ Python/Lista_Recursao/resolucoes/exc6.py
@@ -9,11 +9,11 @@ def busca_maior(array, maior_valor):
         maior_valor = array[0]
     del(array[0])
     if not array:
-        print(maior_valor)
-        return
-    busca_maior(array, maior_valor)
+        return maior_valor
+    else:
+        return busca_maior(array, maior_valor)
 
 
 lista_teste = [0, 1, 3, 17, 10, 5, 4, 10]
 
-busca_maior(lista_teste, -math.inf)
+print(busca_maior(lista_teste, -math.inf))
