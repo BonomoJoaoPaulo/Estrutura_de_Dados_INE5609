94373a8210a4cf3025b9fdfd12a554f35ee85cf9
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-05-11 21:25:14 -0300

    Criação da Fila

---
+++ Python/Caminho_Cavalo/Queue.py
@@ -0,0 +1,36 @@
+class Queue:
+    def __init__(self, size):
+        self.queue = [None] * size
+        self.first = 0
+        self.last = 0
+
+    def insert(self, item):
+        if self.last < len(self.queue):
+            self.queue[self.last] = item
+            self.last += 1
+        else:
+            self.increase_size()
+            self.queue[self.last] = item
+            self.last += 1
+        return
+
+    def pop(self):
+        x = self.queue[self.first]
+        self.first += 1
+        return x
+
+    def increase_size(self):
+        new_queue = [None] * len(self.queue) * 2
+        for index, item in enumerate(self.queue):
+            new_queue[index] = item
+        self.queue = new_queue
+        return
+
+    def size(self):
+        return self.last - self.first
+
+    def front(self):
+        return self.queue[self.first]
+
+    def empty(self):
+        return self.last == self.first
