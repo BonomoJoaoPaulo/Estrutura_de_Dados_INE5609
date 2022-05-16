65374bba3c8016f2e2719e78170cfa2e6db3f5c2
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-05-15 22:01:05 -0300

    Finalização do Problema do Cavalo

--- Python/Caminho_Cavalo/main.py
+++ Python/Caminho_Cavalo/main.py
@@ -3,9 +3,36 @@ from Position import Position
 from Queue import Queue
 
 
-def verified(already_verified, position):
-    for verificado in already_verified:
+def verified(verificados, position):
+    for verificado in verificados:
         if (verificado.column == position.column) and (verificado.row == position.column):
             return True
     return False
 
+
+def acha_menor_caminho(origem, destino):
+    verificados = []
+    fila = Queue(64)
+    fila.insert(origem)
+    while not(fila.empty()):
+        pos_popped = fila.pop()
+        if pos_popped.column == destino.column and pos_popped.row == destino.row:
+            return pos_popped.step
+        possible_positions = ChessHelper.calculate_position(pos_popped)
+        for position in possible_positions:
+            if not verified(verificados, position) and position.is_valid():
+                fila.insert(Position(position.column, position.row, pos_popped.step+1))
+                verificados.append(pos_popped)
+
+    return "Nao encontrado"
+
+
+pos_inicial = input()
+pos_destino = input()
+
+inicio = Position(ChessHelper.get_column_number(pos_inicial[0]), int(pos_inicial[1]) - 1, 0)
+fim = Position(ChessHelper.get_column_number(pos_destino[0]), int(pos_destino[1]) - 1, 0)
+
+steps = acha_menor_caminho(inico, fim)
+
+print(f"Movimentos: {steps}")
