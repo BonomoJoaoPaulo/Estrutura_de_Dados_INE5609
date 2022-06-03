8733f3fc73dac9c4ae2771f912e056a20c45ef46
BonomoJoaoPaulo <jpabonomo@gmail.com>
2022-06-02 21:29:48 -0300

    Lista Duplamente Encadeada

---
+++ Python/Estruturas/Listas/Lista_duplamente_encadeada/DoublyLinkedList.py
@@ -0,0 +1,38 @@
+class DoublyLinked:
+
+    class _Node:
+        def __init__(self, element, prev, nxt):
+            self._element = element
+            self._prev = prev
+            self._next = nxt
+
+    def __init__(self):
+        self._header = self._Node(None, None, None)
+        self._trailer = self._Node(None, None, None)
+        self._header._next = self._trailer                  # trailer is after header
+        self._trailer._prev = self._header                  # header is before trailer
+        self._size = 0
+
+    def insert_between(self, e, predecessor, successor):
+        newest = self._Node(e, predecessor, successor)
+        predecessor._next = newest
+        successor._prev = newest
+        self._size += 1
+        return newest
+
+    def delete_node(self, node):
+        predecessor = node._prev
+        successor = node._next
+        predecessor._next = successor
+        successor._prev = predecessor
+        self._size -= 1
+        element = node._element                             # record deleted element
+        node._prev = node._next = node._element = None      # deprecate node
+        return element
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+ 
